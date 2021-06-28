76c63471e50d024ab48b65bef539e045c5b9d9d1
justinfargnoli <justinfargnoli@gmail.com>
2021-06-28 14:24:57 -0400

    Move code to `SILAlive` dir

--- lib/SILAlive/SILAlive.cpp
+++ lib/SILAlive/SILAlive.cpp
@@ -10,5 +10,81 @@
 //
 //===----------------------------------------------------------------------===//
 
-#include "ir/instr.h"
+#include "llvm_util/llvm2alive.h"
 
+#include "llvm/Analysis/TargetLibraryInfo.h"
+
+#include "swift/AST/IRGenOptions.h"
+#include "swift/AST/IRGenRequests.h"
+#include "swift/Basic/PrimarySpecificPaths.h"
+#include "swift/SIL/SILModule.h"
+#include "swift/SILAlive/SILAlive.h"
+#include "swift/SILOptimizer/PassManager/Passes.h"
+#include "swift/Subsystems.h"
+#include "swift/TBDGen/TBDGen.h"
+
+using namespace swift;
+
+// Lower the SILModule to the Lowered stage so that it's ready to be 
+// lowered to LLVM IR.
+void lowerSIL(SILModule &SILMod) {
+  switch (SILMod.getStage()) {
+  case SILStage::Raw:
+    assert(!runSILDiagnosticPasses(SILMod));
+    SWIFT_FALLTHROUGH;
+  case SILStage::Canonical:
+    runSILLoweringPasses(SILMod);
+    break;
+  case SILStage::Lowered:
+    break;
+ }
+}
+
+GeneratedModule genIR(std::unique_ptr<SILModule> SILMod) {
+  // Lower \p SILMod to SILStage::Lowered
+  lowerSIL(*SILMod.get());
+
+  // Generate LLVM IR for the \p SILMod
+  llvm::GlobalVariable *HashGlobal;
+  return performIRGeneration(
+      SILMod->getSwiftModule(), IRGenOptions{}, TBDGenOptions{}, std::move(SILMod),
+      /*ModuleName=*/ "", PrimarySpecificPaths{},
+      /*parallelOutputFilenames=*/ {}, &HashGlobal);
+}
+
+std::optional<IR::Function> aliveIRGen(llvm::Function &F, llvm::Triple triple) {
+  return llvm_util::llvm2alive(F, 
+      llvm::TargetLibraryInfo{llvm::TargetLibraryInfoImpl{triple}}, 
+      std::vector<std::string_view>());
+}
+
+bool SILAliveLLVM(SILModule *M) { 
+  std::unique_ptr<SILModule> SILMod(M);
+
+  // Generate LLLVM IR for the SILModule
+  GeneratedModule generatedModule = genIR(std::move(SILMod));
+  llvm::Module *IRMod = generatedModule.getModule();
+  assert(IRMod && "IR module generation failed.");
+
+  // TODO: Remove
+  llvm::errs() << "\n- BEFORE dump\n";
+  IRMod->dump(); 
+  llvm::errs() << "\n- AFTER dump\n";
+
+  // The version of this tool that translated SIL to Alive IR directly 
+  // will be a \c SILFunctionTransform instead of a \c SILModuleTransform.
+  // So, for simplicity, only consider the first function.
+  auto begin = IRMod->begin();
+  assert(begin != IRMod->end() && 
+      "The must be at least one function in the IR module.");
+  llvm::Function *function = &*begin;
+
+  // Lower LLVM IR to Alive IR
+  auto result = aliveIRGen(*function, 
+      generatedModule.getTargetMachine()->getTargetTriple());
+
+  // Store Alive IR in ASTContext
+  // TODO
+
+  return true; 
+}
--- lib/SILOptimizer/UtilityPasses/AliveIRGen.cpp
+++ lib/SILOptimizer/UtilityPasses/AliveIRGen.cpp
@@ -10,73 +10,16 @@
 //
 //===----------------------------------------------------------------------===//
 
-#include "swift/AST/IRGenOptions.h"
-#include "swift/AST/IRGenRequests.h"
-#include "swift/Basic/PrimarySpecificPaths.h"
-#include "swift/SIL/SILModule.h"
-#include "swift/SILOptimizer/PassManager/Passes.h"
+#include "swift/SILAlive/SILAlive.h"
 #include "swift/SILOptimizer/PassManager/Transforms.h"
-#include "swift/Subsystems.h"
-#include "swift/TBDGen/TBDGen.h"
 
 using namespace swift;
 
 namespace {
 
 class AliveIRGen : public SILModuleTransform {
-  llvm::Module *genIR(std::unique_ptr<SILModule> SILMod) {
-    TBDGenOptions TBDGenOpts{};
-    IRGenOptions IRGenOpts{};
-    PrimarySpecificPaths primarySpecificPaths{};
-    llvm::GlobalVariable *HashGlobal;
-
-    GeneratedModule generatedModule = performIRGeneration(
-        SILMod->getSwiftModule(), IRGenOpts, TBDGenOpts, std::move(SILMod),
-        /*ModuleName=*/ "", primarySpecificPaths,
-        /*parallelOutputFilenames=*/ {}, &HashGlobal);
-
-    // TODO: Remove
-    llvm::errs() << "\n- BEFORE performIRGeneration dump\n";
-    generatedModule.getModule()->dump();
-    llvm::errs() << "\n- AFTER performIRGeneration dump\n";
-
-    return generatedModule.getModule();
-  }
-
-  void run() override {
-    std::unique_ptr<SILModule> SILMod(getModule());
-
-    switch (SILMod->getStage()) {
-    case SILStage::Raw:
-      assert(!runSILDiagnosticPasses(*SILMod.get()));
-      SWIFT_FALLTHROUGH;
-    case SILStage::Canonical:
-      runSILLoweringPasses(*SILMod.get());
-      break;
-    case SILStage::Lowered:
-      break;
-    }
-
-    // Generate LLLVM IR for the SILModule
-    llvm::Module *IRMod = genIR(std::move(SILMod));
-    assert(IRMod && "IR module generation failed.");
-
-    // TODO: Remove
-    llvm::errs() << "\n- BEFORE dump\n";
-    IRMod->dump(); // TODO remove
-    llvm::errs() << "\n- AFTER dump\n";
-
-    // The version of this tool that translated SIL to Alive IR directly 
-    // will be a \c SILFunctionTransform instead of a \c SILModuleTransform.
-    // So, for simplicity, only consider the first function.
-    llvm::Function *function = IRMod->getFunction("main");
-
-    // Lower LLVM IR to Alive IR
-    // TODO
-
-    // Store Alive IR in ASTContext
-    // TODO
-  }
+  void run() override { 
+    assert(!SILAliveLLVM(getModule())); }
 };
 
 } // end anonymous namespace
